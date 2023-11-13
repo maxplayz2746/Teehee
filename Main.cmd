65664040db4a9dab05ee7df5e67b21231effba66
maxplayz7566 <84992927+maxplayz2746@users.noreply.github.com>
2023-11-12 22:11:47 -0700

    Create Main.cmd

---
+++ Main.cmd
@@ -0,0 +1,92 @@
+@echo off
+
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+echo Hee Good Luck
+
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://drive.google.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://drive.google.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://google.ca
+start "" https://drive.google.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://open.spotify.com
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+MainRuntime.cmd
